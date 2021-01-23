274acd4edd833d4cf0da3f174524bef8600952ab
Brad Ross (FG-AM-63) <Brad.Ross@bmwmc.com>
2021-01-23 17:48:36 -0500

    initial push

---
+++ middleware/middleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"local/auth-svc/auth"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := auth.TokenValid(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, "unauthorized")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
